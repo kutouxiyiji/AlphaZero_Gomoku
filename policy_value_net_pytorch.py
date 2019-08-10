1d7b599efb684276e161eed2f41440126f756bc1
kutouxiyiji <30358622+kutouxiyiji@users.noreply.github.com>
2019-08-10 15:23:01 -0700

    Update policy_value_net_pytorch.py

--- policy_value_net_pytorch.py
+++ policy_value_net_pytorch.py
@@ -145,7 +145,7 @@ class PolicyValueNet():
         entropy = -torch.mean(
                 torch.sum(torch.exp(log_act_probs) * log_act_probs, 1)
                 )
-        return loss.data[0], entropy.data[0]
+        return loss.item(), entropy.item()
         #for pytorch version >= 0.5 please use the following line instead.
         #return loss.item(), entropy.item()
 
